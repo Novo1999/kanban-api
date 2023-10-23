17ad1ada720d05adbee88e3f1df81855688a3271
Novo1999 <neptune.navo@gmail.com>
2023-10-23 10:33:51 +0600

    server

--- server.ts
+++ server.ts
@@ -18,14 +18,14 @@ import mongoSanitize from 'express-mongo-sanitize'
 dotenv.config()
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 const DATABASE_URL = process.env.MONGO_URL!
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
-app.use(express.static(path.resolve(__dirname, '../public')))
+// app.use(express.static(path.resolve(__dirname, '../public')))
 app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
@@ -35,9 +35,9 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/kanban', authenticateUser as () => void, kanbanRouter)
 app.use('/api/v1/users', authenticateUser as () => void, userRouter)
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
+// })
 
 app.use('*', (req, res) => {
   res.status(StatusCodes.NOT_FOUND).json({ msg: 'not found' })
